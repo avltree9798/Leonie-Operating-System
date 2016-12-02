33e7f45b9d72b4cc98ebc40600155a66a08b2906
root <root@localhost.localdomain>
2016-12-03 01:39:17 +0700

    Adding comment to ASM

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,12 +1,12 @@
 bits  32
 section         .text
-        align   4
-        dd      0x1BADB002
-        dd      0x00
-        dd      - (0x1BADB002+0x00)
+        align   4           ;4 bytes
+        dd      0x1BADB002  ;magic number buat bootloader
+        dd      0x00        ;flags
+        dd      - (0x1BADB002+0x00) ;checksumnya, m+f+c harus = 0
 global start
 extern kmain
 start:
-        cli
-        call kmain
-        hlt
+        cli     ;bersihin interrupt
+        call kmain ;panggil fungsinya
+        hlt         ;halt cpu
